9bfc1e9a7c14a395cca97a5bc3274f9594306997
alok676875 <74966945+alok676875@users.noreply.github.com>
2020-12-01 02:40:14 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -23,13 +23,13 @@ sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Debian Linux And Paste It Down\n'
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
 su - ALOK -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ '
+printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
-    printf "\nUpgrade Complete " >&2
+    printf "\n\nUpgrade Completed " >&2
 else
-    printf "\nError Occured " >&2
+    printf "\n\nError Occured " >&2
 fi
